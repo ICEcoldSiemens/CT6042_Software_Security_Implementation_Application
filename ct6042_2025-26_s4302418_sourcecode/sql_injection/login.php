4415554c8152f7d25904cb5021a6442d45089aa6
ICEcoldShallom <somtoezeoke.develop@gmail.com>
2025-12-18 13:44:22 +0000

    feat(sql-inject): adding vulnerable php code

---
+++ ct6042_2025-26_s4302418_sourcecode/sql_injection/login.php
@@ -0,0 +1,17 @@
+<?php
+$conn = mysqli_connect("localhost", "root", "", "mock_sql_injection");
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+// SQL query vulnerable to SQL injection
+$sql_query = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
+$result = mysqli_query($conn, $sql_query);
+
+if (mysqli_num_rows($result) > 0) {
+ echo "Login successful. Welcome, " . $username;
+} else {
+ echo "Invalid credentials!";
+}
+
+?>
